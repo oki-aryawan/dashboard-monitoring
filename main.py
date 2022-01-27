382a16252d45ca78357bfc24d0326197edf21caa
Leak27 <oki271002@gmail.com>
2022-01-27 09:22:07 +0800

    boiled plate

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-if __name__ = '__main__':
+"""
+
+Aplikasi utama
+
+"""
+
+
+def extrac_data():
+    pass
+
+
+def data_display(result):
+    pass
+
+
+if __name__ == "__main__" :
     print('Aplikasi Utama')
+    
+    result = extrac_data()
+    data_display (result)
 
 
 
